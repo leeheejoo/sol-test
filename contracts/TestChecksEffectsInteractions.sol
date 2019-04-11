9895c5b899bd182e53b8b0bb3809302c384c71fd
lhj <lhj@bitcash.com>
2019-04-11 10:26:11 +0900

    array return test

--- contracts/TestChecksEffectsInteractions.sol
+++ contracts/TestChecksEffectsInteractions.sol
@@ -3,17 +3,35 @@ pragma solidity >=0.4.21 <0.6.0;
 contract TestChecksEffectsInteractions {
 
     mapping(address => uint) balances;
+    address[] addresses;
+    uint[] values;
 
     function deposit() public payable {
         balances[msg.sender] = msg.value;
+        addresses.push(msg.sender);
+        values.push(msg.value);
     }
 
     function balance() public view returns (uint) {
         return balances[msg.sender];
     }
 
+    // get dynamic array
+    function balanceArray() public view returns (address[] memory, uint[] memory) {
+
+        address[] memory arrAddr = new address[](addresses.length);
+        uint[] memory arrValues = new uint[](values.length);
+
+        for (uint i = 0; i < addresses.length; i++){
+            arrAddr[i] = addresses[i];
+            arrValues[i] = values[i];
+        }
+
+        return (arrAddr, arrValues);
+    }
+
     function withdraw(uint amount) public {
-        require(balances[msg.sender] >= amount, 'The balance is less than withdraw amount!');
+        require(balances[msg.sender] >= amount, 'The balance is less than withdraw amount!!');
 
         balances[msg.sender] -= amount;
 
--- test/test_cei.js
+++ test/test_cei.js
@@ -2,7 +2,7 @@
 const TestChecksEffectsInteractions = artifacts.require("../contracts/TestChecksEffectsInteractions.sol");
 
 let Contract;
-let ContractAddr = "0x8145c054572Af2daE8Af7540F1ba03cE0973C8b1";
+let ContractAddr = "0x22E1931Bc1601bD4027E0816B713BA543cD82A80";
 
 contract("Test", async function(accounts) {
     
@@ -13,23 +13,36 @@ contract("Test", async function(accounts) {
     })
 
     describe('test#1', function () {
-        it("basic Test", async () => {
 
-            let amount = web3.utils.toWei("1", "ether");
+        // it("basic Test", async () => {
 
-            let ret = await Contract.deposit({ from: accounts[0], value:amount });
+        //     let amount = web3.utils.toWei("1", "ether");
 
-            let balance = await Contract.balance({ from: accounts[0]});
+        //     let ret = await Contract.deposit({ from: accounts[0], value:amount });
 
-            console.log(`contract balance is ${balance}`);
+        //     let balance = await Contract.balance({ from: accounts[0]});
 
-            ret = await Contract.withdraw(amount, { from: accounts[0]});
+        //     console.log(`contract balance is ${balance}`);
 
-           // balance = await Contract.balance({ from: accounts[0]});
-           // console.log(`contract balance is ${balance}`);
+        //     ret = await Contract.withdraw(amount, { from: accounts[0]});
+
+        //     balance = await Contract.balance({ from: accounts[0]});
+        //     console.log(`contract balance is ${balance}`);
             
-        });
+        // });
 
+        it("get array Test", async () => {
+
+            for(let i = 0; i < 10; i++){
+                let amount = web3.utils.toWei(i.toString(), "wei");
+                let ret = await Contract.deposit({ from: accounts[i], value:amount });
+            }
+
+            let ret = await Contract.balanceArray({ from: accounts[0]});
+
+            console.log(`contract address is ${ret[0]}`);
+            console.log(`contract balance is ${ret[1]}`);
+        });
     });
 
 });
